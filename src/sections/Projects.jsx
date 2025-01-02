5d15f48ae6cf68afe5785ab6a9cf573926700e09
wilson <wilsonliu202@gmail.com>
2025-01-01 19:57:06 -0500

    Expand btn for projects and skills

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 import ProjectCard from "../components/projects/ProjectCard";
 import Divider from "../components/Divider";
@@ -10,111 +10,120 @@ import binyc from "../assets/binyc.png";
 import dietfirst from "../assets/diet-first.png";
 import taskManager from "../assets/taskManager.png";
 
-const containerVariants = {
-  hidden: { opacity: 0 },
-  visible: {
-    opacity: 1,
-    transition: {
-      staggerChildren: 0.3,
-    },
-  },
-};
-
 const Projects = () => {
-  return (
-    <motion.section
-      id="projects"
-      className="p-5 pt-11"
-      initial="hidden"
-      animate="visible"
-      variants={containerVariants}
-    >
-      <Divider text="projects" />
-      <ProjectCard
-        image={dietfirst}
-        title="DietFirst"
-        description="Created a personalized diet recommendation app as part of CUNY Tech Prep and a class project. Features include customizable recipe filtering, health progress tracking, and seamless user experience designed to promote healthier lifestyles."
-        technologies={[
-          "MongoDB",
-          "Node.js",
-          "Express.js",
-          "React.js",
-          "Tailwind CSS",
-          "Edamam API",
-        ]}
-        repoLink="https://github.com/DietFirst/DietFirst"
-        demoLink="https://dietfirst-frontend.vercel.app/"
-      />
+  const [isExpanded, setIsExpanded] = useState(false);
 
-      <ProjectCard
-        image={binyc}
-        title="BINyc"
-        description="Created a real-time map using the MERN stack and Leaflet for reporting trash hotspots, enhancing community-driven waste management. Developed at Hack Knight Queens College."
-        technologies={[
-          "MongoDB",
-          "ExpressJS",
-          "ReactJS",
-          "NodeJS",
-          "LeafletJS",
-          "Tailwind CSS",
-        ]}
-        repoLink="https://github.com/Aziiimm/BINyc"
-        demoLink="https://devpost.com/software/binyc?ref_content=my-projects-tab&ref_feature=my_projects"
-      />
-      <ProjectCard
-        image={ecoisland}
-        title="EcoIsland"
-        description="Developed a reusable container tracking system for Governors Island using Next.js and Supabase, promoting sustainability through container usage tracking."
-        technologies={["Next.js", "Supabase", "ShadcnUI", "TypeScript"]}
-        repoLink="https://github.com/dchen024/fitch-hackathon-fall-24"
-        demoLink="https://ecoisland.vercel.app/"
-      />
-      <ProjectCard
-        image={wander}
-        title="Wander"
-        description="Built a decentralized travel platform with React and MongoDB, featuring personalized travel maps. Won Best Use of MongoDB Atlas at Cornell BigRed//Hacks."
-        technologies={[
-          "React.js",
-          "MongoDB",
-          "Node.js",
-          "Express.js",
-          "Tailwind CSS",
-          "Pinata Cloud",
-          "Google Maps API",
-        ]}
-        repoLink="https://github.com/wilsonliu2/wander"
-        demoLink="https://devpost.com/software/wander-rs9m76"
-      />
-      <ProjectCard
-        image={cunyCompass}
-        title="LA'CUNY Compass"
-        description="Created a resource tool for CUNY students using OpenAI and React, awarded Best Use of Data Science at CTP Hacks."
-        technologies={[
-          "React.js",
-          "OpenAI API",
-          "Flask",
-          "Leaflet.js",
-          "Tailwind CSS",
-        ]}
-        repoLink="https://github.com/crosve/CTP-Hack"
-        demoLink="https://ctp-hack.vercel.app/"
-      />
-      <ProjectCard
-        image={nycHealthMap}
-        title="NYC Health Map"
-        description="Developed an interactive health data map of NYC using JavaScript and Leaflet, highlighting health trends across boroughs."
-        technologies={["JavaScript", "D3", "Leaflet", "HTML", "CSS"]}
-        repoLink="https://github.com/wilsonliu2/nyc-health-map"
-        demoLink="https://wilsonliu2.github.io/nyc-health-map/"
-      />
-      <ProjectCard
-        image={taskManager}
-        title="Task Maanger"
-        description="A simple todo list application built using React and Spring Boot with MySQL for task management. Features include task categorization, marking tasks as completed, and a responsive UI."
-        technologies={["React", "Tailwind CSS", "Spring Boot", "MySQL"]}
-        repoLink="https://github.com/wilsonliu2/fullstack-todo"
-        demoLink="https://github.com/wilsonliu2/fullstack-todo"
-      />
+  const handleToggle = () => {
+    setIsExpanded((prev) => !prev);
+  };
+
+  return (
+    <motion.section id="projects" className="p-5 pt-11">
+      <div className="flex items-center justify-between">
+        <Divider text="projects" />
+        <button
+          onClick={handleToggle}
+          className="font-thin text-neutral-400 hover:underline focus:outline-none"
+        >
+          {isExpanded ? "Collapse" : "Expand"}
+        </button>
+      </div>
+      <motion.div
+        initial={{ height: 0, opacity: 0 }}
+        animate={
+          isExpanded
+            ? { height: "auto", opacity: 1 }
+            : { height: 0, opacity: 0 }
+        }
+        transition={{ duration: 0.3 }}
+        className="overflow-hidden"
+      >
+        <ProjectCard
+          image={dietfirst}
+          title="DietFirst"
+          description="Created a personalized diet recommendation app as part of CUNY Tech Prep and a class project. Features include customizable recipe filtering, health progress tracking, and seamless user experience designed to promote healthier lifestyles."
+          technologies={[
+            "MongoDB",
+            "Node.js",
+            "Express.js",
+            "React.js",
+            "Tailwind CSS",
+            "Edamam API",
+          ]}
+          repoLink="https://github.com/DietFirst/DietFirst"
+          demoLink="https://dietfirst-frontend.vercel.app/"
+        />
+
+        <ProjectCard
+          image={binyc}
+          title="BINyc"
+          description="Created a real-time map using the MERN stack and Leaflet for reporting trash hotspots, enhancing community-driven waste management. Developed at Hack Knight Queens College."
+          technologies={[
+            "MongoDB",
+            "ExpressJS",
+            "ReactJS",
+            "NodeJS",
+            "LeafletJS",
+            "Tailwind CSS",
+          ]}
+          repoLink="https://github.com/Aziiimm/BINyc"
+          demoLink="https://devpost.com/software/binyc?ref_content=my-projects-tab&ref_feature=my_projects"
+        />
+        <ProjectCard
+          image={ecoisland}
+          title="EcoIsland"
+          description="Developed a reusable container tracking system for Governors Island using Next.js and Supabase, promoting sustainability through container usage tracking."
+          technologies={["Next.js", "Supabase", "ShadcnUI", "TypeScript"]}
+          repoLink="https://github.com/dchen024/fitch-hackathon-fall-24"
+          demoLink="https://ecoisland.vercel.app/"
+        />
+        <ProjectCard
+          image={wander}
+          title="Wander"
+          description="Built a decentralized travel platform with React and MongoDB, featuring personalized travel maps. Won Best Use of MongoDB Atlas at Cornell BigRed//Hacks."
+          technologies={[
+            "React.js",
+            "MongoDB",
+            "Node.js",
+            "Express.js",
+            "Tailwind CSS",
+            "Pinata Cloud",
+            "Google Maps API",
+          ]}
+          repoLink="https://github.com/wilsonliu2/wander"
+          demoLink="https://devpost.com/software/wander-rs9m76"
+        />
+        <ProjectCard
+          image={cunyCompass}
+          title="LA'CUNY Compass"
+          description="Created a resource tool for CUNY students using OpenAI and React, awarded Best Use of Data Science at CTP Hacks."
+          technologies={[
+            "React.js",
+            "OpenAI API",
+            "Flask",
+            "Leaflet.js",
+            "Tailwind CSS",
+          ]}
+          repoLink="https://github.com/crosve/CTP-Hack"
+          demoLink="https://ctp-hack.vercel.app/"
+        />
+        <ProjectCard
+          image={nycHealthMap}
+          title="NYC Health Map"
+          description="Developed an interactive health data map of NYC using JavaScript and Leaflet, highlighting health trends across boroughs."
+          technologies={["JavaScript", "D3", "Leaflet", "HTML", "CSS"]}
+          repoLink="https://github.com/wilsonliu2/nyc-health-map"
+          demoLink="https://wilsonliu2.github.io/nyc-health-map/"
+        />
+        <ProjectCard
+          image={taskManager}
+          title="Task Manager"
+          description="A simple todo list application built using React and Spring Boot with MySQL for task management. Features include task categorization, marking tasks as completed, and a responsive UI."
+          technologies={["React", "Tailwind CSS", "Spring Boot", "MySQL"]}
+          repoLink="https://github.com/wilsonliu2/fullstack-todo"
+          demoLink="https://github.com/wilsonliu2/fullstack-todo"
+        />
+      </motion.div>
     </motion.section>
   );
 };
--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -47,6 +47,7 @@ const Skills = () => {
   const sectionRef = useRef(null);
   const isInView = useInView(sectionRef);
   const [animationTrigger, setAnimationTrigger] = useState(false);
+  const [isExpanded, setIsExpanded] = useState(false);
 
   useEffect(() => {
     if (isInView) {
@@ -56,158 +57,173 @@ const Skills = () => {
     }
   }, [isInView]);
 
+  const handleToggle = () => {
+    setIsExpanded((prev) => !prev);
+  };
+
   return (
     <section id="skills" ref={sectionRef}>
       <div className="p-5 pt-11">
-        <Divider text="proficient skills" />
-        <div className="mt-4 flex justify-between">
-          {/* Frontend Development */}
-          <motion.div
-            className="w-1/2 pr-4"
-            variants={containerVariants}
-            initial="hidden"
-            animate={animationTrigger ? "visible" : "hidden"}
+        <div className="flex items-center justify-between">
+          <Divider text="proficient skills" />
+          <button
+            onClick={handleToggle}
+            className="font-thin text-neutral-400 hover:underline focus:outline-none"
           >
-            <h3 className="text-lg font-light text-neutral-400">
-              Frontend Development
-            </h3>
-            <motion.ul
-              className="mt-2 grid grid-cols-2 gap-4 text-neutral-300"
-              variants={containerVariants}
-              initial="hidden"
-              animate={animationTrigger ? "visible" : "hidden"}
-            >
-              <motion.li variants={itemVariants}>
-                <Icon icon={FaReact} label="React.js" />
-              </motion.li>
-              <motion.li variants={itemVariants}>
-                <Icon icon={SiNextdotjs} label="Next.js" />
-              </motion.li>
-              <motion.li variants={itemVariants}>
-                <Icon icon={SiTailwindcss} label="Tailwind CSS" />
-              </motion.li>
-              <motion.li variants={itemVariants}>
-                <Icon icon={SiFramer} label="Framer Motion" />
-              </motion.li>
-              <motion.li variants={itemVariants}>
-                <Icon icon={FaHtml5} label="HTML" />
-              </motion.li>
-              <motion.li variants={itemVariants}>
-                <Icon icon={FaCss3Alt} label="CSS" />
-              </motion.li>
-            </motion.ul>
-          </motion.div>
-
-          {/* Backend & Databases */}
-          <motion.div
-            className="w-1/2 border-l border-neutral-700 pl-4"
-            variants={containerVariants}
-            initial="hidden"
-            animate={animationTrigger ? "visible" : "hidden"}
-          >
-            <h3 className="text-lg font-light text-neutral-400">
-              Backend & Databases
-            </h3>
-            <motion.ul
-              className="mt-2 grid grid-cols-2 gap-4 text-neutral-300"
-              variants={containerVariants}
-              initial="hidden"
-              animate={animationTrigger ? "visible" : "hidden"}
-            >
-              <motion.li variants={itemVariants}>
-                <Icon icon={SiSpringboot} label="Spring Boot" />
-              </motion.li>
-              <motion.li variants={itemVariants}>
-                <Icon icon={SiNodedotjs} label="Node.js" />
-              </motion.li>
-              <motion.li variants={itemVariants}>
-                <Icon icon={SiExpress} label="Express.js" />
-              </motion.li>
-              <motion.li variants={itemVariants}>
-                <Icon icon={SiMongodb} label="MongoDB" />
-              </motion.li>
-              <motion.li variants={itemVariants}>
-                <Icon icon={SiPostgresql} label="PostgreSQL" />
-              </motion.li>
-              <motion.li variants={itemVariants}>
-                <Icon icon={SiMysql} label="MySQL" />
-              </motion.li>
-              <motion.li variants={itemVariants}>
-                <Icon icon={SiFlask} label="Flask" />
-              </motion.li>
-              <motion.li variants={itemVariants}>
-                <Icon icon={SiSupabase} label="Supabase" />
-              </motion.li>
-            </motion.ul>
-          </motion.div>
+            {isExpanded ? "Collapse" : "Expand"}
+          </button>
         </div>
-
-        <div className="mt-8 flex justify-between border-t border-neutral-700 pt-8">
-          {/* Tools & Frameworks */}
-          <motion.div
-            className="w-1/2 pr-4"
-            variants={containerVariants}
-            initial="hidden"
-            animate={animationTrigger ? "visible" : "hidden"}
-          >
-            <h3 className="text-lg font-light text-neutral-400">
-              Tools & Technologies
-            </h3>
-            <motion.ul
-              className="mt-2 grid grid-cols-2 gap-4 text-neutral-300"
+        <motion.div
+          initial={{ height: 0, opacity: 0 }}
+          animate={
+            isExpanded
+              ? { height: "auto", opacity: 1 }
+              : { height: 0, opacity: 0 }
+          }
+          transition={{ duration: 0.3 }}
+          className="overflow-hidden"
+        >
+          <div className="mt-4 flex justify-between">
+            {/* Frontend Development */}
+            <motion.div
+              className="w-1/2 pr-4"
               variants={containerVariants}
               initial="hidden"
               animate={animationTrigger ? "visible" : "hidden"}
             >
-              <motion.li variants={itemVariants}>
-                <Icon icon={FaGitAlt} label="Git" />
-              </motion.li>
-              <motion.li variants={itemVariants}>
-                <Icon icon={SiLeaflet} label="Leaflet.js" />
-              </motion.li>
-              <motion.li variants={itemVariants}>
-                <Icon icon={SiFramer} label="Framer Motion" />
-              </motion.li>
-              <motion.li variants={itemVariants}>
-                <Icon icon={FaGitAlt} label="GitHub" />
-              </motion.li>
-            </motion.ul>
-          </motion.div>
+              <h3 className="text-lg font-light text-neutral-400">
+                Frontend Development
+              </h3>
+              <motion.ul
+                className="mt-2 grid grid-cols-2 gap-4 text-neutral-300"
+                variants={containerVariants}
+              >
+                <motion.li variants={itemVariants}>
+                  <Icon icon={FaReact} label="React.js" />
+                </motion.li>
+                <motion.li variants={itemVariants}>
+                  <Icon icon={SiNextdotjs} label="Next.js" />
+                </motion.li>
+                <motion.li variants={itemVariants}>
+                  <Icon icon={SiTailwindcss} label="Tailwind CSS" />
+                </motion.li>
+                <motion.li variants={itemVariants}>
+                  <Icon icon={SiFramer} label="Framer Motion" />
+                </motion.li>
+                <motion.li variants={itemVariants}>
+                  <Icon icon={FaHtml5} label="HTML" />
+                </motion.li>
+                <motion.li variants={itemVariants}>
+                  <Icon icon={FaCss3Alt} label="CSS" />
+                </motion.li>
+              </motion.ul>
+            </motion.div>
 
-          {/* Programming Languages */}
-          <motion.div
-            className="w-1/2 border-l border-neutral-700 pl-4"
-            variants={containerVariants}
-            initial="hidden"
-            animate={animationTrigger ? "visible" : "hidden"}
-          >
-            <h3 className="text-lg font-light text-neutral-400">
-              Programming Languages
-            </h3>
-            <motion.ul
-              className="mt-2 grid grid-cols-2 gap-4 text-neutral-300"
+            {/* Backend & Databases */}
+            <motion.div
+              className="w-1/2 border-l border-neutral-700 pl-4"
               variants={containerVariants}
               initial="hidden"
               animate={animationTrigger ? "visible" : "hidden"}
             >
-              <motion.li variants={itemVariants}>
-                <Icon icon={FaJava} label="Java" />
-              </motion.li>
-              <motion.li variants={itemVariants}>
-                <Icon icon={FaJs} label="JavaScript" />
-              </motion.li>
-              <motion.li variants={itemVariants}>
-                <Icon icon={SiTypescript} label="TypeScript" />
-              </motion.li>
-              <motion.li variants={itemVariants}>
-                <Icon icon={SiPython} label="Python" />
-              </motion.li>
-              <motion.li variants={itemVariants}>
-                <Icon icon={SiPython} label="SQL" />
-              </motion.li>
-            </motion.ul>
-          </motion.div>
-        </div>
+              <h3 className="text-lg font-light text-neutral-400">
+                Backend & Databases
+              </h3>
+              <motion.ul
+                className="mt-2 grid grid-cols-2 gap-4 text-neutral-300"
+                variants={containerVariants}
+              >
+                <motion.li variants={itemVariants}>
+                  <Icon icon={SiSpringboot} label="Spring Boot" />
+                </motion.li>
+                <motion.li variants={itemVariants}>
+                  <Icon icon={SiNodedotjs} label="Node.js" />
+                </motion.li>
+                <motion.li variants={itemVariants}>
+                  <Icon icon={SiExpress} label="Express.js" />
+                </motion.li>
+                <motion.li variants={itemVariants}>
+                  <Icon icon={SiMongodb} label="MongoDB" />
+                </motion.li>
+                <motion.li variants={itemVariants}>
+                  <Icon icon={SiPostgresql} label="PostgreSQL" />
+                </motion.li>
+                <motion.li variants={itemVariants}>
+                  <Icon icon={SiMysql} label="MySQL" />
+                </motion.li>
+                <motion.li variants={itemVariants}>
+                  <Icon icon={SiFlask} label="Flask" />
+                </motion.li>
+                <motion.li variants={itemVariants}>
+                  <Icon icon={SiSupabase} label="Supabase" />
+                </motion.li>
+              </motion.ul>
+            </motion.div>
+          </div>
+
+          <div className="mt-8 flex justify-between border-t border-neutral-700 pt-8">
+            {/* Tools & Frameworks */}
+            <motion.div
+              className="w-1/2 pr-4"
+              variants={containerVariants}
+              initial="hidden"
+              animate={animationTrigger ? "visible" : "hidden"}
+            >
+              <h3 className="text-lg font-light text-neutral-400">
+                Tools & Technologies
+              </h3>
+              <motion.ul
+                className="mt-2 grid grid-cols-2 gap-4 text-neutral-300"
+                variants={containerVariants}
+              >
+                <motion.li variants={itemVariants}>
+                  <Icon icon={FaGitAlt} label="Git" />
+                </motion.li>
+                <motion.li variants={itemVariants}>
+                  <Icon icon={SiLeaflet} label="Leaflet.js" />
+                </motion.li>
+                <motion.li variants={itemVariants}>
+                  <Icon icon={SiFramer} label="Framer Motion" />
+                </motion.li>
+                <motion.li variants={itemVariants}>
+                  <Icon icon={FaGitAlt} label="GitHub" />
+                </motion.li>
+              </motion.ul>
+            </motion.div>
+
+            {/* Programming Languages */}
+            <motion.div
+              className="w-1/2 border-l border-neutral-700 pl-4"
+              variants={containerVariants}
+              initial="hidden"
+              animate={animationTrigger ? "visible" : "hidden"}
+            >
+              <h3 className="text-lg font-light text-neutral-400">
+                Programming Languages
+              </h3>
+              <motion.ul
+                className="mt-2 grid grid-cols-2 gap-4 text-neutral-300"
+                variants={containerVariants}
+              >
+                <motion.li variants={itemVariants}>
+                  <Icon icon={FaJava} label="Java" />
+                </motion.li>
+                <motion.li variants={itemVariants}>
+                  <Icon icon={FaJs} label="JavaScript" />
+                </motion.li>
+                <motion.li variants={itemVariants}>
+                  <Icon icon={SiTypescript} label="TypeScript" />
+                </motion.li>
+                <motion.li variants={itemVariants}>
+                  <Icon icon={SiPython} label="Python" />
+                </motion.li>
+                <motion.li variants={itemVariants}>
+                  <Icon icon={SiPython} label="SQL" />
+                </motion.li>
+              </motion.ul>
+            </motion.div>
+          </div>
+        </motion.div>
       </div>
     </section>
   );
